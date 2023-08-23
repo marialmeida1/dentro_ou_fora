517cde9f10f34afb0537181791666a8dcf2b3d0d
Mariana <marianaalmeidafga@gmail.com>
2023-08-23 11:09:17 -0300

    criando home para cadastros

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,98 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Thefacebook | Welcome to Thefacebook!</title>
-    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
-</head>
-<body>
-    <center>
-        <table class="bordertable" cellspacing="0" cellpadding="0" border="0" width="700">
-          <tr><td>
-              <table class="bottomborder" cellspacing="0" cellpadding="0" border="0" width="100%">
-              <tr><td width="350" bgcolor="#3B5998">
-                <img src="{{  asset('img/logo-left.jpg') }}"></td>
-                  <td bgcolor="#3B5998"><table cellspacing="0" cellpadding="0" border="0" width="100%"><tr><td>
-                  <table cellspacing="0" cellpadding="0" border="0" width="100%">
-                  <tr><td><a href="/"><img src="{{ asset('img/logo-right.jpg') }}" border="0"></a></td>
-                  <td width="100%" bgcolor="#3B5998">&nbsp;</td></tr></table></td></tr>
-                  <tr><td><table cellspacing="0" cellpadding="4" border="0" width="100%"><tr height="21">
-                  <!--<td bgcolor=#3B5998 width=10>&nbsp;</td>-->
-                  <td bgcolor="#3B5998" valign="bottom">&nbsp;<a class="menu" href="{{ route('login') }}">login</a></td>
-                  <td bgcolor="#3B5998" valign="bottom">&nbsp;<a class="menu" href="{{ route('register') }}">register</a></td>
-                  <td bgcolor="#3B5998" valign="bottom">&nbsp;<a class="menu" href="#">about</a></td>
-                  <td bgcolor="#3B5998" width="100%">&nbsp;</td>
-                  </tr></table></td>
-                  </tr></table>
-              </td></tr></table>
-          </td></tr>
-          <tr><td><table cellspacing="0" cellpadding="2" border="0" width="100%">
-              <tr><td valign="top">
-              <table cellspacing="0" cellpadding="0" border="0" width="105">
-                <tr><td>
-                                    <table class="dashedtable" cellspacing="0" cellpadding="2" width="100%">
-                      <tr><td align="right">
-                          <p>
-                        @auth
-                          <input type="button" class="inputsubmit" value="home" onclick="javascript:document.location='{{ route('home') }}';">
-                        @else
-                          <form method="post" action="{{ route('login') }}">
-                            @csrf
-                            Email:<br>
-                            <input type="text" class="inputtext" name="email"><br>
-                            @error('email') <font color="red">{{ $message }}</font> @enderror
-                            Password:<br>
-                            <input type="password" class="inputtext" name="password"><br>
-                            @error('password') <font color="red">{{ $message }}</font> @enderror
-                            <center><input type="button" class="inputsubmit" value="register" onclick="javascript:document.location='{{ route('register') }}';">
-                                  &nbsp;<input type="submit" class="inputsubmit" value="login"></center>
-                          </form>
-                        @endauth
-                        <br>
-                      </td></tr></table>
-                           </td></tr>
-              </table>
-              </td><td width="595" valign="top">
-                <table class="bordertable" cellspacing="0" cellpadding="0" border="1" width="100%"><tr><td>
+@extends('layouts.app')
 
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
 
-        &nbsp;<center><p class="title">[ Login ]<p>
-        <form method="post" action="{{ route('login') }}">
-            @csrf
-            <input type="hidden" name="page" value="">
-            <table cellspacing="0" cellpadding="2" border="0">
-            <tr><td>Email:</td>
-            <td>
-                <input type="text" class="inputtext" name="email" value="" size="30">
-                @error('email') <font color="red">{{ $message }}</font> @enderror
-            </td></tr>
-            <tr><td>Password:</td>
-            <td>
-                <input type="password" class="inputtext" name="password" size="30">
-                @error('password') <font color="red">{{ $message }}</font> @enderror
-            </td></tr>
-            <tr height="7"><td></td><td></td></tr>
-            </table>
-            <input type="submit" value="   Login   " class="inputsubmit">&nbsp;&nbsp;
-            <input type="button" value="Register" class="inputsubmit" onclick="javascript:document.location='{{ route('register') }}';">
-        </form>
-        <br>
-        <br>
-        </center>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-          </td></tr></table>
-          </td></tr></table>
-          <center>
-          <p><a href="#">about</a>&nbsp;&nbsp;
-          <a href="#">contact</a>&nbsp;&nbsp;
-          <a href="#">faq</a>&nbsp;&nbsp;
-          <a href="#">terms</a>&nbsp;&nbsp;
-          <a href="#">privacy</a>
-          <br>a IFMG/3rd year 2023 production
-          <br>Thefacebook &copy; 2004
-          </center><br>
-          </td></tr></table>
-</body>
-</html>
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
